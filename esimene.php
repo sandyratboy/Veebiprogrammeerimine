1f5f7b087b3d1a4663840abbe41617bb956e547c
Alexander Lawrence <sandyratboy@users.noreply.github.com>
2017-09-11 10:38:23 +0300

    HTML koodile lisatud esimene php osa

---
+++ esimene.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Alexander Lawrence veebiprogemise asjad</title>
+</head>
+<body>
+    <h1>Alexander Lawrence</h1>
+    <p>See veebileht on loodud veebiprogrammerimise kursusel ning ei sisalda mingisugust tõsiseltvõetavat sisu.</p>
+	<p>Olen tubli tark ja ilus!</p>
+	
+	<?php
+	    echo "Täna on vastik ilm!";
+	?>
+
+</body>
+</html>
